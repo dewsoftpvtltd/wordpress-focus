64ee2d865aef224ec2aa4b6ba5335733be6ced68
dewsoftpvtltd <abdulbasitlangrial@outlook.com>
2017-05-08 11:43:09 +0500

    Other people's money is dirty and smelly

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Work Remaining</title>
+<style>
+body {
+    background-color: white;
+}
+div {
+  font-family: verdana;
+  padding: 5px;
+
+}
+h1 {
+    color: red;
+    text-align: center;
+    padding: 5px;
+
+}
+
+ol li {
+  padding: 5px;
+}
+</style>
+</head>
+<body>
+<div>
+<h1>FOCUS</h1>
+<ol>
+<li>Education : nursery, playgroup, academy, primary school, high school, college, university</li>
+<li>Wedding: Shaadi, mangni, Engagement, Kids, Expecting</li>
+<li>Churches: churches, mosques, cynagoges, mandir, temple</li>
+<li>Transport:rickshaw, loader, taxi, truck, aeroplane, boat, launch</li>
+</ol>
+
+
+<h1>Things to learn </h1>
+<ol>
+<li>Wordpress</li>
+  <li>Moodle</li>
+  <li>eCommerce</li>
+</ol>
+
+</div>
+</body>
+</html>
